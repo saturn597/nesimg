5bebe119cf6240d44be14af16852ee39ff1dc2f7
Jeremy <saturn5971@gmail.com>
2019-05-05 12:10:47 -0400

    Allow user to click a cell to change the color
    Also, experimenting with grid.

--- js/script.js
+++ js/script.js
@@ -1,17 +1,23 @@
 window.addEventListener('load', () => {
     Vue.component('color-palette', {
+        props: {
+            updateColor: Function,
+        },
         template: `
             <div class="palette">
                 <div class="colorOption"
                     v-for="color in this.$options.palette"
                     v-bind:style="{ backgroundColor: color.hex }"
                     v-bind:key="color.id"
-                    v-bind:title="color.hex">
+                    v-bind:title="color.hex"
+                    v-on:click="updateColor(color.hex)">
                 </div>
             </div>
         `,
 
-        palette,
+        palette: hexColors.map((hex, id) => {
+            return { hex, id };
+        }),
     });
 
     Vue.component('drawing-area', {
@@ -24,8 +30,11 @@ window.addEventListener('load', () => {
         },
         methods: {
             update: function(id) {
-                this.pixels[id].color = "#ffff00";
+                this.pixels[id].color = this.currentColor;
             },
+        },
+        props: {
+            currentColor: String,
         },
         template: `
             <div id="drawing">
@@ -40,11 +49,20 @@ window.addEventListener('load', () => {
     });
 
     const app = new Vue({
+        data: {
+            currentColor: "#58F898",
+        },
         el: "#app",
+        methods: {
+            updateColor: function(color) {
+                this.currentColor = color;
+            }
+        },
         template: `
-            <div>
-                <color-palette></color-palette>
-                <drawing-area></drawing-area>
+            <div id='app'>
+                <div id="colorIndicator" v-bind:style="{ backgroundColor: currentColor }"></div>
+                <color-palette v-bind:updateColor="updateColor"></color-palette>
+                <drawing-area v-bind:currentColor="currentColor"></drawing-area>
             </div>
         `,
     });
@@ -65,7 +83,3 @@ const hexColors =
 "#A4E4FC", "#B8B8F8", "#D8B8F8", "#F8B8F8", "#F8A4C0", "#F0D0B0", "#FCE0A8",
 "#F8D878", "#D8F878", "#B8F8B8", "#B8F8D8", "#00FCFC", "#F8D8F8", "#000000",
 "#000000",];
-
-const palette = hexColors.map((hex, id) => {
-    return { hex, id };
-});
